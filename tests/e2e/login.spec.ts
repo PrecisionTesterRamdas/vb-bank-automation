f0256f9f503b92969ec5cc35bac0d63a75a000ea
Ramdas <ramdas.qualityfirst@gmail.com>
2026-04-22 01:44:08 +0530

    refactor: enhance login tests by utilizing isValid flag for test execution

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -5,18 +5,27 @@ import loginData from "../../test-data/login.json";
 test.use({ storageState: { cookies: [], origins: [] } });
 
 for (const data of loginData) {
-  test(`${data.loginSuccessFlag} user login test for ${data.description}`, async ({
-    page,
-    loginPage,
-  }) => {
-    await loginPage.navigate();
-    await loginPage.login(data.username, data.password);
-    if (data.loginSuccessFlag === "Successful") {
+  if (data.isValid === true) {
+    test(`${data.loginSuccessFlag} user login test for ${data.description}`, async ({
+      page,
+      loginPage,
+    }) => {
+      await loginPage.navigate();
+      await loginPage.login(data.username, data.password);
       await expect(page).toHaveURL(/.*dashboard/);
-    } else if (data.loginSuccessFlag === "Unsuccessful") {
+    });
+  }
+
+  if (data.isValid === false) {
+    test(`${data.loginSuccessFlag} user login test for ${data.description}`, async ({
+      page,
+      loginPage,
+    }) => {
+      await loginPage.navigate();
+      await loginPage.login(data.username, data.password);
       await expect(page.getByTestId("alert-error")).toHaveText(
         "Invalid username or password",
       );
-    }
-  });
+    });
+  }
 }
